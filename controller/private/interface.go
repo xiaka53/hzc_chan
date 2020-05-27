eb8473e92d33d7ac7c2f482fd798c37feb9ee4a9
angus <xiaka53@vip.qq.com>
2020-05-27 18:27:59 +0800

    接口备注

--- controller/private/interface.go
+++ controller/private/interface.go
@@ -2,12 +2,14 @@ package private
 
 import "api/dao"
 
+//余额
 type balance interface {
 	getBalance(address, token string) (balance float64)
 	getBalanceAll(token string) (balance float64)
 }
 
-type address interface {
+//账号
+type account interface {
 	newAccount() (address, keys, mnemonic string)
 	importRawKey(key string) (address string)
 	lockAccount(address string) bool
@@ -15,6 +17,7 @@ type address interface {
 	listAccount() (data []string)
 }
 
+//交易
 type transaction interface {
 	sendTransaction(from, to, date string, value, gas float64)
 	getTransactionByHash(hx string) (hash dao.Hash)
@@ -22,12 +25,14 @@ type transaction interface {
 	getTransactionByAddress(address string) (data []dao.Hash)
 }
 
+//挖矿
 type miner interface {
 	start(threads int) bool
 	stop() bool
 	setBase(address string) bool
 }
 
+//代币
 type token interface {
 	address() (data []dao.Token)
 	deploy()
