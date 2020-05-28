ea32a2ad752596408e7c12c80c4240138a08fb71
angus <xiaka53@vip.qq.com>
2020-05-28 17:58:43 +0800

    接口文件公有

--- controller/interface/interface.go
+++ controller/interface/interface.go
@@ -3,37 +3,37 @@ package _interface
 import "api/dao"
 
 //余额
-type balance interface {
-	getBalance(address, token string) (balance float64)
-	getBalanceAll(token string) (balance float64)
+type Balance interface {
+	GetBalance(address, token string) (balance float64)
+	GetBalanceAll(token string) (balance float64)
 }
 
 //账号
-type account interface {
-	newAccount() (address, keys, mnemonic string)
-	importRawKey(key string) (address string)
-	lockAccount(address string) bool
-	unlockAccount(address string) bool
-	listAccount() (data []string)
+type Account interface {
+	NewAccount() (address, keys, mnemonic string)
+	ImportRawKey(key string) (address string)
+	LockAccount(address string) bool
+	UnlockAccount(address string) bool
+	ListAccount() (data []string)
 }
 
 //交易
-type transaction interface {
-	sendTransaction(from, to, date string, value, gas float64)
-	getTransactionByHash(hx string) (hash dao.Hash)
-	getTransactionByBlock(block int) (data []dao.Hash)
-	getTransactionByAddress(address string) (data []dao.Hash)
+type Transaction interface {
+	SendTransaction(from, to, date string, value, gas float64)
+	GetTransactionByHash(hx string) (hash dao.Hash)
+	GetTransactionByBlock(block int) (data []dao.Hash)
+	GetTransactionByAddress(address string) (data []dao.Hash)
 }
 
 //挖矿
-type miner interface {
-	start(threads int) bool
-	stop() bool
-	setBase(address string) bool
+type Miner interface {
+	Start(threads int) bool
+	Stop() bool
+	SetBase(address string) bool
 }
 
 //代币
-type token interface {
-	address() (data []dao.Token)
-	deploy()
+type Token interface {
+	Address() (data []dao.Token)
+	Deploy()
 }
